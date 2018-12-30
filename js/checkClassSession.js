b0beda02ece2b1fa08ce68108c7add7db99c07e1
valiantkui <528227496@qq.com>
2018-12-30 21:16:04 +0800

    add some

--- js/checkClassSession.js
+++ js/checkClassSession.js
@@ -1,12 +1,20 @@
 //alert("checkClassSession");
-
+$.ajaxSetup({
+    xhrFields: {
+        //允许带上凭据
+        withCredentials: true
+    },
+    crossDomain: true,
+});
 var classInfo = null;
 
     $.ajax({
         url: "http://localhost:8880/login/checkClassSession",
         dataType:"text",
         success:function(result){
-            if(result == "") window.location="SignInUp.html";
+            if(result == "") {
+                window.location="SignInUp.html";
+            }
             else {
 
                 classInfo = $.parseJSON(result);
