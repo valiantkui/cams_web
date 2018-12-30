c37f645e480718eadf6f364ff8a036725630ee14
valiantkui <528227496@qq.com>
2018-12-30 15:27:12 +0800

    完成班级后台管理中的添加学生，删除学生

--- js/checkClassSession.js
+++ js/checkClassSession.js
@@ -1,22 +1,17 @@
 //alert("checkClassSession");
-$.ajaxSetup({
-    xhrFields: {
-        //允许带上凭据
-        withCredentials: true
-    },
-    crossDomain: true,
-});
+
 var classInfo = null;
 
     $.ajax({
         url: "http://localhost:8880/login/checkClassSession",
         dataType:"text",
         success:function(result){
-            //alert(result);
-           classInfo = $.parseJSON(result);
-            $("#classInfo").text(classInfo.name+"已登陆");
-           /*alert(classInfo.name);
-           alert(classInfo.school);*/
+            if(result == "") window.location="SignInUp.html";
+            else {
+
+                classInfo = $.parseJSON(result);
+                $("#classInfo").text(classInfo.name+"已登陆");
+            }
         },
         error:function (e) {
             alert("错误");
